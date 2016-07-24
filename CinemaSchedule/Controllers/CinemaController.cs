0dec3d74b10e43dac6f8083173412b82bf5e23b4
Dmitry <Dmitry@DMITRY>
2016-07-25 01:25:27 +0300

    +edit/delete just completed

--- CinemaSchedule/Controllers/CinemaController.cs
+++ CinemaSchedule/Controllers/CinemaController.cs
@@ -1,4 +1,5 @@
 ﻿using CinemaSchedule.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,29 +26,82 @@ namespace CinemaSchedule.Controllers
         {
             var movies = _cinemaDb.Sessions.Where(a => a.Date == data.DateTime).GroupBy(t => t.Theater).ToList();
 
-            data.Cinema = new List<TheaterData>();
+
+            data.Cinema = BuildTheaterData(movies);
 
 
-            foreach (var group in movies)
-            {
-                data.Cinema.Add(new TheaterData()
-                {
-                    Theater = group.Key,
-                    Movies = new List<MovieData>()
-                    {
-                        new MovieData()
-                        {
-                            MovieTitle = group.Select(a=>a.Movie).FirstOrDefault(),
-                            MovieSessions = group.Select(a=>a.Time).ToList()
-                        }
-                    }
-                    // MoviesTitles = group.ToList().GroupBy(a => a.Movie).ToList(),
-
-                });
-            }
             //  = _cinemaDb.Sessions.Where(a => a.Date == data.DateTime).ToList();
 
             return View("List", data);
+        }
+
+        public ActionResult AddMovie(BigViewModel cat)
+        {
+            var model = new BigViewModel();
+            if (cat.SelectedDataModel != null)
+            {
+                var session = new Sessions()
+                {
+                    Theater = cat.SelectedDataModel.SelectedTheater,
+                    Movie = cat.SelectedDataModel.SelectedMovie,
+                    Date = cat.SelectedDataModel.Date,
+                    Time = cat.SelectedDataModel.Time
+                };
+
+                _cinemaDb.Sessions.Add(session);
+                _cinemaDb.SaveChanges();
+                return View("List");
+            }
+            model.CinemaCatalogModel = new CinemaCatalogModel()
+            {
+                Theaters = _cinemaDb.Theaters.ToList(),
+                Movies = _cinemaDb.Movies.ToList(),
+            };
+
+            return View(model);
+        }
+
+        public ActionResult EditMovie(string session)
+        {
+            var res = JsonConvert.DeserializeObject<BaseData>(session);
+
+            return View(res);
+        }
+
+        public ActionResult DeleteMovie(BaseData session)
+        {
+           var data = new CinemaData();
+            //var movies = _cinemaDb.Sessions.Where(a => a.Theater == session.Theater && a.Movie == session.Movie && a.Date == session.Date).GroupBy(a => a.Theater).ToList();
+            //data.Cinema = BuildTheaterData(movies);
+            //data.DateTime = session.Date;
+            return View(data);
+        }
+
+        public List<TheaterData> BuildTheaterData(List<IGrouping<string, Sessions>> movies)
+        {
+            var data = new List<TheaterData>();
+            foreach (var group in movies)
+            {
+                var movs = new List<MovieData>();
+                var groupMovies = group.Select(a => a.Movie).Distinct();
+
+                foreach (var g in groupMovies)
+                {
+                    movs.Add(new MovieData()
+                    {
+                        MovieTitle = g,
+                        MovieSessions = group.Where(b => b.Movie == g).Select(a => a.Time.ToString()).ToList()
+                    });
+                }
+
+                data.Add(new TheaterData()
+                {
+                    Theater = group.Key,
+                    Movies = movs
+
+                });
+            }
+            return data;
         }
     }
 }
--- CinemaSchedule/Models/CinemaData.cs
+++ CinemaSchedule/Models/CinemaData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,16 +12,34 @@ namespace CinemaSchedule.Models
         public List<TheaterData> Cinema { get; set; }
     }
 
+    public class BaseData
+    {
+        [JsonProperty("Theater")]
+        public string Theater { get; set; }
+        [JsonProperty("MovieData")]
+        public MovieData MovieData { get; set; }
+        [JsonProperty("SelectedSession")]
+        public string SelectedSession { get; set; }
+
+        [JsonProperty("Date")]
+        public string Date { get; set; }
+    }
+
     public class TheaterData
     {
+   
+        [JsonProperty("Theater")]
         public string Theater { get; set; }
+        [JsonProperty("Movies")]
         public List<MovieData> Movies { get; set; }
 
     }
 
     public class MovieData
     {
+        [JsonProperty("MovieTitle")]
         public string MovieTitle { get; set; }
-        public List<TimeSpan> MovieSessions { get; set; }
+        [JsonProperty("MovieSessions")]
+        public List<string> MovieSessions { get; set; }
     }
 }
